f26881d18367b185fe8a896bda06d6e35e02a645
Fernando Azevedo <azevedo.fma@gmail.com>
2020-10-29 13:49:48 -0300

    Basic configurations

---
+++ src/pages/_app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../styles/tailwind.css';
+
+const App = ({ Component, pageProps }) => (
+  <Component {...pageProps} />
+);
+
+export default App;
---
+++ src/pages/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => (
+  <h1 className="text-red-500 p-10">Home page with next.js and tailwind</h1>
+);
+
+export default Home;
